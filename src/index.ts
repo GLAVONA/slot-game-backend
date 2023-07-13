5c593cf3797ce713807fe26965289ae0c3ed4776
VK <97589269+GLAVONA@users.noreply.github.com>
2023-07-13 15:35:28 +0300

    implemented prompt-sync

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import SlotMachineConfig from "./config/index";
 import SlotMachine from "./services/slotMachine";
 
+const prompt = require("prompt-sync")({ sigint: true });
+
 export const slotMachine = new SlotMachine(SlotMachineConfig);
 
 // Please provide your number of spins to the spin method
-slotMachine.spin(1);
+while (true) {
+  const numberOfSpins = prompt("Number of spins: ");
+  if (isNaN(numberOfSpins) || numberOfSpins === 0) {
+    console.log("Please enter a valid number of spins!");
+  } else {
+    console.time("Time to complete spins");
+    slotMachine.spin(numberOfSpins);
+    console.log("*********************");
+    console.timeEnd("Time to complete spins");
+    console.log("*********************");
+  }
+}
